6d862c9291a88800a49990319c98a83bd487e5c2
wansang <wansang93@naver.com>
2019-12-03 03:35:01 +0900

    191203 | 027

--- python_100_quizzes/README.md
+++ python_100_quizzes/README.md
@@ -1,4 +1,5 @@
 # python 100 quizzes
+- [027 딕셔너리 만들기](/python_100_quizzes/027_딕셔너리_만들기.md)
 - [026 행성 문제2](/python_100_quizzes/026_행성_문제2.md)
 - [025 원의 넓이를 구하세요](/python_100_quizzes/025_원의_넓이를_구하세요.md)
 - [024 대문자로 바꿔주세요!](/python_100_quizzes/024_대문자로_바꿔주세요!.md)
--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,5 +1,4 @@
-ko_name = ["수성", "금성", "지구", "화성", "목성", "토성", "천왕성", "해왕성"]
-en_name = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
-
-name = input()
-print(en_name[ko_name.index(name)])
+name = input().split()
+score = map(int, input().split())
+dic = dict(zip(name, score))
+print(dic)
