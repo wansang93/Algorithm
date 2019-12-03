5e5d6c065634738e2fe81ca6b4cc81f2e4f17240
wansang <wansang93@naver.com>
2019-12-04 05:38:00 +0900

    191204 | 036

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,12 +1,3 @@
-def one(n):
-    def two(m):
-        squar = m ** n
-        return squar
-    return two
-
-a = one(2)
-b = one(3)
-c = one(4)
-print(a(10))
-print(b(10))
-print(c(10))
+num = int(input())
+for i in range(1, 10):
+    print(num*i, end=" ")
