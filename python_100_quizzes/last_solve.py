db839b53b4755211f37f691837b50b4fc299a2f9
wansang <wansang93@naver.com>
2019-12-07 05:19:29 +0900

    191207 | 040

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,3 +1,26 @@
-s = input()
-new = s.replace('q', 'e')
-print(new)
+# 입력
+
+# 50 무게제한
+# 5  사람 수
+# 20
+# 20
+# 20
+# 20
+# 20
+
+limit = int(input())
+num = int(input())
+mylist = []
+for i in range(num):
+    mylist.append(int(input()))
+
+count = 0
+sum = 0
+for i in mylist:
+    sum += i
+    count += 1
+    if sum > limit:
+        count -= 1
+        break
+
+print(count)
