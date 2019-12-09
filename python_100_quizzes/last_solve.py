5fb98d43ed82ab488b1b57dce15c369b8ef8f204
wansang <wansang93@naver.com>
2019-12-10 01:42:54 +0900

    191210 | 043

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,1 +1,8 @@
-# test git function on visual studio code
+num = int(input())
+mystr = ""
+while num:
+    # print(num)
+    mystr += str(num % 2)
+    num = int(num / 2)
+mystr = mystr[::-1]
+print(int(mystr))
