8eaeef166590f12a4f5fa93fc129bb9b65c15423
wansang <wansang93@naver.com>
2019-12-17 11:29:05 +0900

    191217 | 058

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,1 +1,13 @@
 mystr = input()
+restr = mystr[::-1]
+newstr = ""
+if len(restr) > 3:
+    for i, c in enumerate(restr):
+        if i % 3 == 0:
+            newstr = c + "," + newstr
+        else:
+            newstr = c + newstr
+else:
+    newstr = mystr + ","
+
+print(newstr[:-1])
