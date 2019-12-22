e3f21d820e1365e79293b6c6822d56b99a962892
wansang <wansang93@naver.com>
2019-12-22 21:17:01 +0900

    191222 | 071

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,21 +1,48 @@
-a = ([1, 2],
-     [2, 4])
-b = ([1, 0],
-     [0, 3])
-    
-# ([1,6], [2,12])
+graph = {
+        'A': set(['B', 'C', 'E']),
+        'B': set(['A']),
+        'C': set(['A']),
+        'D': set(['E', 'F']),
+        'E': set(['A', 'D']),
+        'F': set(['D'])
+}
 
-def solution(a, b):
-    c = []
-    if len(a) == len(b[0]):
-        for i in range(len(a)):
-            c.append([0]*len(b[0]))
-        for i in range(len(c)):
-            for j in range(len(c[i])):
-                for k in range(len(a[i])):
-                    c[i][j] += a[i][k] * b[k][j]
-        return c
-    else:
-        return -1
+def dfs(graph, start):
+    visited = []
+    stack = [start]
 
-print(solution(a, b))
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack += graph[n] - set(visited)
+    return visited
+
+print(dfs(graph, 'E'))
+
+
+tree = {
+        'A': ['B', 'C', 'E'],
+        'B': ['A'],
+        'C': ['A'],
+        'D': ['E', 'F'],
+        'E': ['A', 'D'],
+        'F': ['D']
+}
+
+# 왼쪽
+
+def treedfs(tree, start):
+    visited = []
+    stack = [start]
+
+    while stack:
+        n = stack.pop()
+        if n not in visited:
+            visited.append(n)
+            stack += tree[n] - set(visited)
+
+    return visited
+
+
+print(treedfs(graph, 'E'))
