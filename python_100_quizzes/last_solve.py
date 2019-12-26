5bb25dcb2624568b5d8af7b5d7afebb8cf12c9d3
wansang <wansang93@naver.com>
2019-12-27 02:47:18 +0900

    191227 | 077

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,12 +1,26 @@
-string = input()
-rule = input()
-answer = 0
+def lcs(x, y):
+    m=len(x)
+    n=len(y)
 
-n = 0
-while string:
-    count = 0
+    #Syntax is column*rows for memoization table for m*n matrix
+    L=[[None]*(n+1) for i in range(m+1)]
+    
+    #Loop over the memoization table
+    for i in range(m+1):
+        for j in range(n+1):
+            #Initializing first row and cloumn of memoization table
+            if i == 0 or j == 0:
+                L[i][j]=0
+            #Diagonal condtion if equal
+            elif x[i-1] == y[j-1]:
+                L[i][j] = L[i-1][j-1]+1
+            #Diagonal if value is not equal
+            else:
+                L[i][j] = max(L[i-1][j],L[i][j-1])
+    
+    #The final resullt
+    return L[m][n]
 
-    if string != rule[i]
-    if answer < n:
-        answer = n
-        n = 0
+x=input("First string : ")
+y=input("Second string: ")
+print("The length of LCS is : ",lcs(x,y))
