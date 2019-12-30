de470ed9498835559425a2e7394dac3ec7d73ca7
wansang <wansang93@naver.com>
2019-12-30 21:19:10 +0900

    191230 | 078

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,29 +1,26 @@
-# n = int(input())
-# k = int(input())
-
-a = input().split(' ')
-n, k = a
+# a = input().split(' ')
+# n, k = a
 
 
-def sol(n, k):
-    i = 0
-    #q에 n만큼의 숫자를 넣어준다
-    q = [i for i in range(1,n+1)]
+# def sol(n, k):
+#     i = 0
+#     #q에 n만큼의 숫자를 넣어준다
+#     q = [i for i in range(1,n+1)]
 
-    while len(q) > 2:
-        if i > len(q)-1:
-            while i > len(q)-1:
-        #순환하다 i가 q의 길이보다 클 경우에 len(q)만큼 빼준다.
-        #[1,2,3,4,5,6] -> 1다음 4가 빠지고 그 다음은 4+3 = 7(index : 6)이 빠져야 하는데 
-        #i(현재 빠져야 할 index)가 len(q)보다 크므로 len(q)즉, 4를 빼준다. 
-        #이걸 마지막 2개가 남을 때 까지 반복함
-                i -= len(q)
-        q.pop(i)
-        i += k
-        i -= 1
-    print(q)
+#     while len(q) > 2:
+#         if i > len(q)-1:
+#             while i > len(q)-1:
+#         #순환하다 i가 q의 길이보다 클 경우에 len(q)만큼 빼준다.
+#         #[1,2,3,4,5,6] -> 1다음 4가 빠지고 그 다음은 4+3 = 7(index : 6)이 빠져야 하는데 
+#         #i(현재 빠져야 할 index)가 len(q)보다 크므로 len(q)즉, 4를 빼준다. 
+#         #이걸 마지막 2개가 남을 때 까지 반복함
+#                 i -= len(q)
+#         q.pop(i)
+#         i += k
+#         i -= 1
+#     print(q)
 
-sol(int(n),int(k))
+# sol(int(n),int(k))
 
 def solution(n, k):
     mylist = [i for i in range(1, n+1)]
@@ -45,4 +42,5 @@ def solution(n, k):
     return list(set(mylist)-set(eating))
 
 
-print(solution(int(n), int(k)))
+n, k = map(int, input().split())
+print(solution(n, k))
