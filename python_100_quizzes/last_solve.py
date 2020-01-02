352d92955a74c3bb9222d3189a2945ba0090a8d1
wansang <wansang93@naver.com>
2020-01-03 00:10:28 +0900

    200103 | 082

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,90 +1,34 @@
+# 데이터 입력(1), 프로그램 종료(2) : 1
+# 데이터를 입력하세요: 3 + 5
+# True
 
-input_data = '''0 1 0 0 0
-0 0 0 0 0
-0 0 0 1 0
-0 0 1 0 0
-0 0 0 0 0'''
+# 데이터 입력(1), 프로그램 종료(2) : 1
+# 데이터를 입력하세요: 5 + 7) * (3 * 5)
+# False
 
-# output_data = '''* f * 0 0
-# 0 * 0 * 0
-# 0 0 * f *
-# 0 * f * 0
-# 0 0 * 0 0'''
+def math(e):
+    is_ok = True
+    stack = 0
 
-# flag = [] #지뢰 없이 깃발만 있는 리스트
-# minesweeper = [] #지뢰를 찾은 리스트
-# for i in range(5):
-#     flag.append(input('깃발 값과 함께 입력하세요 :').split(' '))
+    for c in e:
+        if c == '(':
+            stack += 1
+        elif c == ')':
+            stack -= 1
+        
+        if stack == -1:
+            is_ok = False
+            break
 
-# minesweeper = [[str(0) for i in range(len(flag))] for i in range(len(flag[0]))]
-
-# for i in range(len(flag)):
-#     for j in range(len(flag[i])):
-#         if flag[i][j] == '1':
-#             minesweeper[i][j] = 'f'
-#             if j-1 >= 0:
-#                 minesweeper[i][j-1] = '*'
-#             if j+1 <= len(flag[i]):
-#                 minesweeper[i][j+1] = '*'
-#             if i-1 >= 0:
-#                 minesweeper[i-1][j] = '*'
-#             if i+1 <= len(flag[i+1]):
-#                 minesweeper[i+1][j] = '*'
-
-# for i in minesweeper:
-#     print(' '.join(i))
-
-
-# 풀이81-2
-
-# value ='''0 1 0 0 0
-# 0 0 0 0 0
-# 0 0 0 1 0
-# 0 0 1 0 0
-# 0 0 0 0 0'''
-# print(value.split('\n'))
-# sp = value.split('\n')
-# count = 0
-# for i in sp:
-#     sp[count] = i.replace('1', 'f').split(' ')
-#     count += 1
-# print(sp)
-# count = 0
-# search = 0
-# for s in sp:
-#     for i in s:
-#         if i == 'f':
-#             search = s.index(i)
-#             if search > 0:
-#                 s[search-1] = '*'
-#             if search < 4:
-#                 s[search+1] = '*'
-#             if count > 0:
-#                 sp[count-1][search] = '*'
-#             if count < 4:
-#                 sp[count+1][search] = '*'
-#     count += 1
-# for i in sp:
-#     print(i)
-
-# 풀이81-3
-
-inputList = []
-for i in range(5):
-    inputList.extend(input().split())
-for i in range(25):
-    if inputList[i] == "1":
-        inputList[i] = "f"
-        if i // 5 != 0:
-            inputList[i - 5] = "*"
-        if i // 5 != 4:
-            inputList[i + 5] = "*"
-        if i % 5 != 0:
-            inputList[i - 1] = "*"
-        if i % 5 != 4:
-            inputList[i + 1] = "*"
-for i in range(25):
-    if i % 5 != 4:
-        print(inputList[i], end = " ")
+    if stack == 0 and is_ok == True:
+        return True
     else:
-        print(inputList[i], " ")
+        return False
+
+while 1:
+    order = input('데이터 입력(1), 프로그램 종료(2): ')
+    if order == '1':
+        ex = input('데이터를 입력하세요: ')
+        print(math(ex))
+    else:
+        break
