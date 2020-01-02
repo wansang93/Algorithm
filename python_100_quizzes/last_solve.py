69e9488520f80913920820f0c4b972e1a8f1de69
wansang <wansang93@naver.com>
2020-01-02 21:24:17 +0900

    200102 | 080

--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,29 +1,31 @@
-# ㄱ,ㄴ,ㄷ,ㄹ
-# 3
+# # ㄱ,ㄴ,ㄷ,ㄹ
+# # 3
 
-# 출력
+# # 출력
 
-# ['ㄱㄴㄷ', 'ㄱㄴㄹ', 'ㄱㄷㄹ', 'ㄴㄷㄹ']
-# 4
+# # ['ㄱㄴㄷ', 'ㄱㄴㄹ', 'ㄱㄷㄹ', 'ㄴㄷㄹ']
+# # 4
 
-import math
+# import math
 
-# mylist = input().split(',')
-# mynum = int(input())
+# # mylist = input().split(',')
+# # mynum = int(input())
 
 
-def combination(inL, inN):
-    newlist = []
-    newnum = int(math.factorial(len(inL)) / ((math.factorial(inN)) * math.factorial(len(inL)-inN)))
+# def combination(inL, inN):
+#     newlist = []
+#     newnum = int(math.factorial(len(inL)) / ((math.factorial(inN)) * math.factorial(len(inL)-inN)))
 
-    for i in range(newnum):
-        newlist.append()
-    print(newlist)
-    print(newnum)
+#     for i in range(newnum):
+#         newlist.append()
+#     print(newlist)
+#     print(newnum)
 
-# print(combination(mylist, mynum))
+# # print(combination(mylist, mynum))
 
-print(combination(['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ'], 4))
+# print(combination(['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ'], 4))
+
+# 풀이 2
 
 # from itertools import permutations # 이 문제에서는 안쓰지만 어떤 것인지 확인해보세요.
 # from itertools import combinations
@@ -36,3 +38,31 @@ print(combination(['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ'], 4))
 # print(list(itertools.combinations(user_input, 3)))
 
 # print(list(map(''.join, combinations(user_input, user_input_int))))
+
+# 풀이 3
+
+mylist = input().split(',')
+mynum = int(input())
+
+def combination2(arr, r):
+    # 1.
+    arr = sorted(arr)
+    all_list = []
+    # 2.
+    def generate(chosen):
+        if len(chosen) == r:
+            # print(chosen)
+            all_list.append(''.join(map(str, chosen)))
+            return
+
+    	# 3.
+        start = arr.index(chosen[-1]) + 1 if chosen else 0
+        for nxt in range(start, len(arr)):
+            chosen.append(arr[nxt])
+            generate(chosen)
+            chosen.pop()
+    generate([])
+    print(all_list)
+    print(len(all_list))
+
+combination2(mylist, mynum)
