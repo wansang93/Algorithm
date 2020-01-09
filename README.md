714b22309046c2a0ad8f721703bc355db50a085f
wansang <wansang93@naver.com>
2020-01-09 16:38:50 +0900

    200109 | 091

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 매일매일 알고리즘을 풀고 기록합니다. 이용하는 사이트는 다음과 같습니다.
 
-## 1. [python 100 quizzes](/python_100_quizzes/)
+## 1. [Python 100 quizzes](/python_100_quizzes/)
 
 [LINK](https://www.notion.so/Python-100-6ee1860ce29a41bc8eb6b9cfa7d7f06c)
 
--- python_100_quizzes/last_solve.py
+++ python_100_quizzes/last_solve.py
@@ -1,9 +1,49 @@
-a = [1,2,3,4]
-b = ['a','b','c','d']
-mylist = []
+import random  as r
 
-for i, j in zip(a, b):
-    mylist.append([i, j])
+# 총 7개, 30명인 학생, 5과목
+class_num = 7
+student_num = 30
+subject_num = 5
 
-mylist = [[i, j] for i, j in zip(a, b)]
-print(mylist)
+# 1. 반 점수 모두가 담긴 전교 점수 다중 리스트를 만들어주세요.
+
+total_score = []
+for i in range(class_num):
+    class_score = []
+    for j in range(student_num):
+        score = []
+        for i in range(subject_num):
+            score.append(r.randint(1, 100))
+        class_score.append(score)
+    total_score.append(class_score)
+print('1번 문제')
+print(total_score)
+
+# 전체 평균, 반 평균, 학생 평균
+total_avg = 0
+class_avg = []
+
+# 2. 반 평균을 구하세요.
+for classes in total_score:
+    student_avg = 0
+    for students in classes:
+        student_avg += sum(students) / subject_num
+    class_avg.append(student_avg / student_num)
+print('2번 문제')
+print(class_avg)
+
+# 3. 반 1등 점수를 구하세요.
+frist_class = []
+for classes in total_score:
+    frist_rank = 0
+    for students in classes:
+        sum_students = sum(students)
+        if frist_rank < sum_students:
+            frist_rank = sum_students
+    frist_class.append(frist_rank)
+print('3번 문제')
+print(frist_class)
+
+# 4. 전교 평균을 구하세요.
+print('4번 문제')
+print(sum(class_avg) / class_num)
