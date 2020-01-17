a0c3c2c12f9aca8fc240fce7f46d1eec91a123c4
wansang <wansang93@naver.com>
2020-01-17 23:04:35 +0900

    200117 | Solve Kakao Quiz

--- Programmers/Python/Code/다트 게임.py
+++ Programmers/Python/Code/다트 게임.py
@@ -1,26 +1,33 @@
 def solution(dartResult):
     answer = 0
+    scorelist = [str(i) for i in range(1, 10)]
     newlist = ['0']
     for i in dartResult:
-        if i in ['1', '2', '3']:
-            newlist.append('+%c' % i)
-        elif i in ['S', 'D', 'T']:
+        if i in scorelist:  # 1~9까지의 수
+            newlist.append('+')
+            newlist.append(i)
+        elif i == '0':  # 0일 때
+            if newlist[-1] == '1':
+                newlist.append(i)
+            else:
+                newlist.append('+')
+                newlist.append(i)
+        elif i in ['S', 'D', 'T']:  # S, D, K 일때
             if i == 'S':
                 newlist.append('**1')
             elif i == 'D':
                 newlist.append('**2')
             elif i == 'T':
                 newlist.append('**3')
-        elif i in ['*', '#']:
+        elif i in ['*', '#']:  # *, # 일때
             if i == '*':
+                if newlist[-3] == '+':
+                    newlist[-3] = '*2+'
                 newlist.append('*2')
             elif i == '#':
                 newlist.append('*(-1)')
-
-    newstr = ''.join(newlist)
-    print(int(newstr))
-
+    answer = eval(''.join(newlist))
     return answer
 
-dartResult = '1S2D*3T'
-print(solution(dartResult))
+# dartResult = '1D2S#10S'
+# print(solution(dartResult))
