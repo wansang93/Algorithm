bfe1c7e922b04f7f30b765bede647807cd77dc7e
wansang <wansang93@naver.com>
2020-01-18 00:24:51 +0900

    200118 | Solve Kakao Quiz

---
+++ Programmers/Python/Code/실패율.py
@@ -0,0 +1,23 @@
+def solution(N, stages):
+    answer = []
+    ss = sorted(stages)
+    total = len(ss)
+    temp = []
+    for i in range(1, N+1):
+        if i in ss:
+            value = ss.count(i)
+            temp.append(value/total)
+            total -= value
+        else:
+            temp.append(0)
+    # print(temp)
+    for i in range(N):
+        pop_idx = temp.index(max(temp))
+        answer.append(pop_idx+1)
+        temp[pop_idx] = -1
+
+    return answer
+
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+
+# sorted(result, key=lambda x : result[x], reverse=True)
--- Programmers/Python/README.md
+++ Programmers/Python/README.md
@@ -20,6 +20,7 @@
 - [[link]](https://programmers.co.kr/learn/courses/30/lessons/60057) [문자열 압축](Code/문자열%20압축.py)
 - [[link]](https://programmers.co.kr/learn/courses/30/lessons/17681) [비밀지도](Code/비밀지도.py)
 - [[link]](https://programmers.co.kr/learn/courses/30/lessons/17682) [다트 게임](Code/다트%20게임.py)
+- [[link]](https://programmers.co.kr/learn/courses/30/lessons/42889) [실패율](Code/실패율.py)
 </details>
 
 <details>
