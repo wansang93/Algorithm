554257f02a78f2141226776f9aa5d151966d6177
wansang <wansang93@naver.com>
2020-07-18 22:26:52 +0900

    200718 | Solve D3 1 Python quiz on Programmers

--- SW Expert Academy/Python/Python D3/2806. N-Queen.py
+++ SW Expert Academy/Python/Python D3/2806. N-Queen.py
@@ -1,15 +1,26 @@
-# 풀이 중
-
-import sys
-sys.stdin = open(r'C:/Users/wansang/Desktop/Gitrep/Algorithm/SW Expert Academy/Python/Python D3/input.txt', 'r')
-
 T = int(input())
 for t in range(1, T+1):
-    N = int(input())    
-    chess_board = [[0 for _ in range(N)] for _ in range(N)]
-    count = 0
-    
 
+    result = 0
+    def adjacent(level):
+        for i in range(level):
+            if row[level] == row[i] or abs(row[level] - row[i]) == level - i:
+                return False
+        return True
+            
+    def dfs(level):
+        global result
+        
+        if level == N:
+            result += 1
 
-    
-    print(f'#{t} {count}')
+        else:
+            for i in range(N):
+                row[level] = i
+                if adjacent(level):
+                    dfs(level + 1)
+
+    N = int(input())
+    row = [0] * N
+    dfs(level=0)
+    print(f'#{t} {result}')
