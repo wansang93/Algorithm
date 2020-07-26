e7226fda3921d1406a6b5f17ef02b065c2c3e751
wansang <wansang93@naver.com>
2020-07-26 23:28:01 +0900

    200726 | Solve D3 1 Python quiz on SWEA

--- SW Expert Academy/Python/Python D3/3809. 화섭이의 정수 나열.py
+++ SW Expert Academy/Python/Python D3/3809. 화섭이의 정수 나열.py
@@ -1,18 +1,31 @@
-# 풀이 중
-
-import sys
-sys.stdin = open(r'C:/Users/wansang/Desktop/Gitrep/Algorithm/SW Expert Academy/Python/Python D3/input.txt', 'r')
+# 참고 URL  [https://jksk0115.tistory.com/25]
 
 T = int(input())
 for t in range(1, T+1):
     N = int(input())
-    nums = list(map(int, input().split()))
+    nums = []
+    while len(nums) < N: 
+        nums += list(input().split())
+    # print(nums)
 
-    answer = 0
-    while True:
-        temp = str(answer)
-        for c in nums:
-            pass
+    answer = -1
+    check_nums = [False] * 10000
+    len_nums = len(nums)
+
+    for digit in range(1, 5):
+        for i in range(N-(digit-1)):
+            check_nums[int(''.join(nums[i:i+digit]))] = True
+            # print(nums[i:i+digit])
+        
+        start = 0 if digit == 1 else 10 ** (digit - 1)
+        end = 10 ** digit
+
+        for i in range(start, end):
+            if not check_nums[i]:
+                answer = i
+                break
+        else:
+            continue
         break
-    
+
     print(f'#{t} {answer}')
