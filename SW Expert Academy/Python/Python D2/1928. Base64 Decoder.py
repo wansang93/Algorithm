1a18e99001408de17d870a1aa4b024aaf32ee0d0
wansang <wansang93@naver.com>
2020-08-02 08:17:15 +0900

    200802 | Resolve a D2 1 Python quiz on SWEA

--- SW Expert Academy/Python/Python D2/1928. Base64 Decoder.py
+++ SW Expert Academy/Python/Python D2/1928. Base64 Decoder.py
@@ -9,45 +9,22 @@ for t in range(1, T+1):
     print(f'#{t} {b64decode(input()).decode("UTF-8")}')
 
 # 풀이2
-# ~~~~~~~~풀이중~~~~~~~~~~
+value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+base_dic = {val: idx for idx, val in enumerate(value)}
+
 def base_decoding(string):
-    '''
-    input: Basic encoded string(string)
-    output: string
-    string to bit
-    bit to string
-    'A' 00    print(ord('A'))  # 65
-    'Z' 25    print(ord('Z'))  # 90
-    'a' 26    print(ord('a'))  # 97
-    'z' 51    print(ord('z'))  # 122
-    '+' 62    print(ord('+'))  # 43
-    '/' 63    print(ord('/'))  # 47
-    '''
     bit = ''
     text = ''
-    # check_number and then number to char
+    for i in string:
+        num = bin(base_dic[i])[2:].zfill(6)
+        bit += num
 
-### 숫자를 만날경우 에러 발생함ㅜㅜ원리를 이해 못함
-### An error occurred here!!! Because I excepted finding number.
-
-    # Basic encoded string -> bit
-    for c in string:
-        if ord('A') <= ord(c) <= ord('Z'):
-            bit += bin(ord(c)-65)[2:].zfill(6)
-        elif ord('a') <= ord(c) <= ord('z'):
-            bit += bin(ord(c)-97+26)[2:].zfill(6)
-        elif c == '+':
-            bit += bin(62)[2:].zfill(6)
-        elif c == '/':
-            bit += bin(63)[2:].zfill(6)
-
-    # bit -> decoded string
-    for i in range(len(bit)//8):
-        text += chr(int(bit[8*i:8*(i+1)], 2))
+    for i in range(0, len(bit), 8):
+        text += chr(int(bit[i:i+8], 2))
 
     return text
 
 T = int(input())
 for t in range(1, T+1):
     text = base_decoding(input())
-    print('#{} {}'.format(t, text))
+    print(f'#{t} {text}')
